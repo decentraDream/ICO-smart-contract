588e720b3eb742923b1b78188f8a46d6df911bd8
mahdiidarabi <mahdi@teleportdao.xyz>
2023-03-26 14:32:57 +0300

    Adding hardhat deploy plugin and hardhat config

---
+++ deploy/001_lock.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { BigNumber } from "ethers"
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts } = hre
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+
+    await deploy("Lock", {
+        from: deployer,
+        log: true,
+        skipIfAlreadyDeployed: true,
+        args: [
+            1000
+        ]
+    })
+}
+
+export default func
+export const tags = ["Lock"]
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,41 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { HttpNetworkUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+		compilers: [
+			{
+				version: "0.8.9",
+				settings: {
+					optimizer: {
+						enabled: true
+					},
+				},
+			}
+		],
+	},
+  paths: {
+    artifacts: "artifacts",
+    deploy: "deploy",
+    deployments: "deployments",
+  },
+  typechain: {
+    outDir: "src/types",
+    target: "ethers-v5",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  etherscan: {
+	  apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
